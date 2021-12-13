360a8276353809a1ef104f915b1a1998cb2ace83
Osiris <potterosiris@gmail.com>
2021-12-13 17:13:20 -0500

    routes

--- finmcdade-client/src/App.js
+++ finmcdade-client/src/App.js
@@ -1,7 +1,48 @@
 import React from "react";
 
+import {
+  BrowserRouter as Router,
+  Redirect,
+  Route,
+  Routes,
+} from "react-router-dom";
+
+import { useAuth } from "./shared/hooks/auth-hook";
+
+import { AuthContext } from "./shared/context/auth-context";
+
 const App = () => {
-  return <div></div>;
+  const { token, login, logout, userId } = useAuth();
+
+  let theRoutes;
+
+  if (token) {
+    theRoutes = (
+      <Routes>
+        <Route path="/dashboard" />
+      </Routes>
+    );
+  }
+  if (!token) {
+    <Routes>
+      <Route path="/" />
+      <Route path="*" />
+    </Routes>;
+  }
+
+  return (
+    <AuthContext.Provider
+      value={{
+        isLoggedIn: !!token,
+        token: token,
+        userId: userId,
+        login: login,
+        logout: logout,
+      }}
+    >
+      <Router>{theRoutes}</Router>
+    </AuthContext.Provider>
+  );
 };
 
 export default App;
